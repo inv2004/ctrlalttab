040ea14ee96609f53f3dc155c83ae72df98ede55
inv2004 <inv2004@gmail.com>
2026-01-20 23:17:40 +0300

    Create ctrlalttab.nim

---
+++ ctrlalttab.nim
@@ -0,0 +1,76 @@
+import winim/lean
+import wNim/[wApp, wFrame]
+import wAuto
+
+type
+  HotkeyData = object
+    hHook: HHOOK
+    lastKeyCode: int
+    lastModifiers: int
+    alttab: bool
+
+var hkData {.threadvar.}: HotkeyData
+
+proc keyProc(nCode: int32, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.} =
+  var processed = false
+  let kbd = cast[LPKBDLLHOOKSTRUCT](lParam)
+  defer:
+    result = if processed: LRESULT 1 else: CallNextHookEx(0, nCode, wParam, lParam)
+
+  case int wParam
+  of WM_KEYUP, WM_SYSKEYUP:
+    hkData.lastKeyCode = 0
+    var isMod = false
+
+    case int kbd.vkCode
+    of VK_LCONTROL, VK_RCONTROL:
+      hkData.lastModifiers = hkData.lastModifiers and (not wModCtrl)
+      isMod = true
+      if hkData.alttab:
+        send "{ENTER}"
+        hkData.alttab = false
+    of VK_LMENU, VK_RMENU: hkData.lastModifiers = hkData.lastModifiers and (not wModAlt); isMod = true
+    of VK_LSHIFT, VK_RSHIFT: hkData.lastModifiers = hkData.lastModifiers and (not wModShift); isMod = true
+    of VK_LWIN, VK_RWIN: hkData.lastModifiers = hkData.lastModifiers and (not wModWin); isMod = true
+    else: discard
+
+  of WM_KEYDOWN, WM_SYSKEYDOWN:
+    case int kbd.vkCode
+
+    of VK_LCONTROL, VK_RCONTROL, VK_LMENU, VK_RMENU, VK_LSHIFT, VK_RSHIFT, VK_LWIN, VK_RWIN:
+      hkData.lastKeyCode = 0
+      case int kbd.vkCode
+      of VK_LCONTROL, VK_RCONTROL: hkData.lastModifiers = hkData.lastModifiers or wModCtrl
+      of VK_LMENU, VK_RMENU: hkData.lastModifiers = hkData.lastModifiers or wModAlt
+      of VK_LSHIFT, VK_RSHIFT: hkData.lastModifiers = hkData.lastModifiers or wModShift
+      of VK_LWIN, VK_RWIN: hkData.lastModifiers = hkData.lastModifiers or wModWin
+      else: discard
+
+    else:
+      let keyCode = int kbd.vkCode
+      var modifiers = 0
+      if hkData.lastModifiers != 0:
+        if (GetAsyncKeyState(VK_CONTROL) and 0x8000) != 0: modifiers = modifiers or wModCtrl
+        if (GetAsyncKeyState(VK_MENU) and 0x8000) != 0: modifiers = modifiers or wModAlt
+        if (GetAsyncKeyState(VK_SHIFT) and 0x8000) != 0: modifiers = modifiers or wModShift
+        if (GetAsyncKeyState(VK_LWIN) and 0x8000) != 0 or (GetAsyncKeyState(VK_RWIN) and 0x8000) != 0:
+          modifiers = modifiers or wModWin
+          hkData.lastModifiers = modifiers
+
+      if keyCode != hkData.lastKeyCode:
+        if modifiers == wModCtrl and keyCode == VK_TAB:
+          send "!{TAB}"
+          processed = true
+          hkData.alttab = true
+
+      hkData.lastKeyCode = keyCode
+
+  else: discard
+
+proc main() =
+  let app = App()
+  discard Frame()
+  SetWindowsHookEx(WH_KEYBOARD_LL, keyProc, GetModuleHandle(nil), 0)
+  app.mainLoop()
+
+main()
